ad7d5d62f7654ab4153af4bdfdc40457e207d82d
Chloe Greene <thecatliferules@gmail.com>
2023-04-26 18:54:35 -0400

    Setup Piechart with basics

--- lib/stats.dart
+++ lib/stats.dart
@@ -1,54 +1,66 @@
 //This file has all the source code for the "add data" section of the app
 //i mean most, not all code
 
-
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
-import 'package:get_it/get_it.dart';
 import 'package:get_it_mixin/get_it_mixin.dart';
 import 'package:sports_analyzer_sta/data_entry.dart';
 
 import 'main.dart';
 
-class Stats extends StatelessWidget with GetItMixin{
-
-
+class Stats extends StatelessWidget with GetItMixin {
   @override
   Widget build(BuildContext context) {
-
     //Points here are refering to datapoints
 
-    final points = watchOnly((DataPoints dp) => dp.points         );
-    final player = watchOnly((GlobalData gd) => gd.selectedPlayer );
+    final points = watchOnly((DataPoints dp) => dp.points);
+    final player = watchOnly((GlobalData gd) => gd.selectedPlayer);
 
     final _playerPoints = points.where((i) => i.player == player).toList();
 
-    final _totalPoints = points.where((i) => i.player == player).toList();
-    final _totalfouls    = _playerPoints.where((p) => p.type == DataPointType.Foul).length;
-    final _totalmiss     = _playerPoints.where((p) => p.type == DataPointType.Miss).length;
-    final _totalbasckets = _playerPoints.where((p) => p.type == DataPointType.Foul).length;
-
+    final _totalPoints = _playerPoints.length;
+    final _totalfouls = _playerPoints
+        .where((p) => p.type == DataPointType.Foul)
+        .length
+        .toDouble();
+    final _totalmiss = _playerPoints
+        .where((p) => p.type == DataPointType.Miss)
+        .length
+        .toDouble();
+    final _totalbasckets = _playerPoints
+        .where((p) => p.type == DataPointType.Foul)
+        .length
+        .toDouble();
 
     return Center(
       child: GridView.count(
         crossAxisCount: 2,
         children: [
-          Text(
-            "Data for player: $player",
-            style: const TextStyle(
-              fontSize: 38
-            ),
-            ),
+          //Data Showing what player it is
+          Column(
+            children: [
+              Text(
+                "Data for player: $player",
+                style: const TextStyle(fontSize: 38),
+              ),
+              Divider()
+            ],
+          ),
           PieChart(
-            PieChartData(
-              sections: [
-                PieChartSectionData(
+            PieChartData(sections: [
+              PieChartSectionData(
+                  title: "Fouls",
                   color: Colors.amber,
-                  value: 100.0 - player 
-                ),
-                
-              ]
-            ),
+                  value: _totalPoints - _totalfouls),
+              PieChartSectionData(
+                  title: "Basket's",
+                  color: Colors.green,
+                  value: _totalPoints - _totalbasckets),
+              PieChartSectionData(
+                  title: "Miss's",
+                  color: Colors.red,
+                  value: _totalPoints - _totalmiss),
+            ]),
             swapAnimationDuration: Duration(milliseconds: 150), // Optional
             swapAnimationCurve: Curves.linear, // Optional
           ),
@@ -59,8 +71,5 @@ class Stats extends StatelessWidget with GetItMixin{
         ],
       ),
     );
-
-
   }
-
 }
